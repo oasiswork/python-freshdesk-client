615a8bf97f3c6331676f90e7d5fc597798a6e2d9
Jocelyn Delalande <jdelalande@oasiswork.fr>
2014-11-07 16:50:35 +0100

    Added readme, license and setup

---
+++ README.md
@@ -0,0 +1,12 @@
+Wrapper arround freshdesk REST API
+===================================
+
+**This work is not officialy supported by Freshdesk**
+
+This code is bare glue to consume Freshdesk REST API, plus some convenience
+methods.
+
+Note that it's partial (covering only Contacts and Customers resources), but
+should be very quick to extend.
+
+Official Freshdesk API doc lives at http://freshdesk.com/api
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+README = file(os.path.join(
+    os.path.dirname(__file__),
+    'README.md')).read()
+
+setup(name='freshdesk-api-client',
+      version='0.1',
+      description='Wrapper arround freshdesk REST API',
+      long_description=README,
+      author='Jocelyn Delalande',
+      author_email='jdelalande@oasiswork.fr',
+      py_modules=['freshdesk_api'],
+      install_requires=['requests'],
+
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+
+          ]
+      )
